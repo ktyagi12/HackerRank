1c617c2538583a5e0cac9d04c5da61e22a3ac2e7
Karishma Tyagi <karishmatyagi12@gmail.com>
2019-11-08 18:40:00 +0000

    Datatypes Program

---
+++ 30DaysOfCode/Day1/DataTypes.py
@@ -0,0 +1,15 @@
+# Problem available at: https://www.hackerrank.com/challenges/30-data-types/problem
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+num1 = int(input())
+num2 = float(input())
+text = input()
+# Print the sum of both integer variables on a new line.
+print(i + num1)
+# Print the sum of the double variables on a new line.
+print(d + num2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + text)
