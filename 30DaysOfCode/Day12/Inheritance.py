80152e27c47d5476c9d3a093c9065aa8d96af626
Karishma Tyagi <karishmatyagi12@gmail.com>
2019-11-12 09:10:16 +0000

    Inheritance

--- 30DaysOfCode/Day12/Inheritance.py
+++ 30DaysOfCode/Day12/Inheritance.py
@@ -1,5 +1,14 @@
 #Problem available at: https://www.hackerrank.com/challenges/30-inheritance/problem
 
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+    def printPerson(self):
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
 class Student(Person):
     #   Class Constructor
     #   
@@ -35,3 +44,12 @@ class Student(Person):
                 return 'E'
             else:
                 return 'O'
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
