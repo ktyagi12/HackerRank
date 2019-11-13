bdf934f9c7fdf2568946897487df58a390b6ae84
Karishma Tyagi <karishmatyagi12@gmail.com>
2019-11-13 08:31:10 +0000

    Basic Exceptions

---
+++ 30DaysOfCode/Day16/Exception.py
@@ -0,0 +1,12 @@
+#Problem available at: https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem
+#!/bin/python3
+
+import sys
+
+
+S = input().strip()
+try:
+    print(int(S))
+except Exception:
+    print('Bad String')
+
