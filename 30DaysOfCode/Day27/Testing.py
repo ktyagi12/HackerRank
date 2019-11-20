86a4d7ec749e9cfb4a7899f7714a161d5be65b85
Karishma Tyagi <karishmatyagi12@gmail.com>
2019-11-20 09:10:54 +0000

    Testing-TestCases

---
+++ 30DaysOfCode/Day27/Testing.py
@@ -0,0 +1,40 @@
+#Problem Available at: https://www.hackerrank.com/challenges/30-testing/problem
+def minimum_index(seq):
+    if len(seq) == 0:
+        raise ValueError("Cannot get the minimum value index from an empty sequence")
+    min_idx = 0
+    for i in range(1, len(seq)):
+        if seq[i] < seq[min_idx]:
+            min_idx = i
+    return min_idx
+class TestDataEmptyArray(object):
+    
+    @staticmethod
+    def get_array():
+        return list()
+        
+        # complete this function
+
+class TestDataUniqueValues(object):
+
+    @staticmethod
+    def get_array():
+        return [5, 2, 8, 3, 1, -6, 9]
+        # complete this function
+
+    @staticmethod
+    def get_expected_result():
+        return 5
+        # complete this function
+
+class TestDataExactlyTwoDifferentMinimums(object):
+
+    @staticmethod
+    def get_array():
+        return [5, 2, 8, 3, 1, -6, 9, -6, 10]
+        # complete this function
+
+    @staticmethod
+    def get_expected_result():
+        return 5
+        # complete this function
