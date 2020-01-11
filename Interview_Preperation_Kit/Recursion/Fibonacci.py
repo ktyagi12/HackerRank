d10fbf50bc549297492618bb1896eca2e0cf3184
Karishma Tyagi <karishmatyagi12@gmail.com>
2020-01-11 12:02:36 +0000

    Fibonacci_Recursion

---
+++ Interview_Preperation_Kit/Recursion/Fibonacci.py
@@ -0,0 +1,12 @@
+#Problem available at: https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=recursion-backtracking
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+n = int(input())
+print(fibonacci(n))
